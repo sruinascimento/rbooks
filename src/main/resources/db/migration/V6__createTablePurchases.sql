642f47ecece4011f86446b23a13159a748b3dd3a
Rui Nascimento <sruy19@gmail.com>
2024-02-21 18:38:49 -0300

    feat: add migration for purchases table

--- src/main/resources/db/migration/V6__createTablePurchases.sql
+++ src/main/resources/db/migration/V6__createTablePurchases.sql
@@ -1,17 +1,18 @@
 CREATE TABLE purchases
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name       VARCHAR(255) NOT NULL,
-    email      VARCHAR(255) NOT NULL,
-    document   VARCHAR(20)  NOT NULL,
+    name       VARCHAR(255)   NOT NULL,
+    email      VARCHAR(255)   NOT NULL,
+    document   VARCHAR(20)    NOT NULL,
     phone      VARCHAR(20),
-    address    VARCHAR(255) NOT NULL,
-    complement VARCHAR(255) NOT NULL,
-    city       VARCHAR(255) NOT NULL,
-    state_id   BIGINT       NOT NULL,
-    country_id BIGINT       NOT NULL,
-    cep        VARCHAR(20)  NOT NULL,
-    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    address    VARCHAR(255)   NOT NULL,
+    complement VARCHAR(255)   NOT NULL,
+    city       VARCHAR(255)   NOT NULL,
+    state_id   BIGINT         NOT NULL,
+    country_id BIGINT         NOT NULL,
+    cep        VARCHAR(20)    NOT NULL,
+    amount     DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT `fk_purchases_state` FOREIGN KEY (state_id) REFERENCES states (id),
     CONSTRAINT fk_purchases_country FOREIGN KEY (country_id) REFERENCES countries (id)
